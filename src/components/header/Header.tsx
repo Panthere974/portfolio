4af95504b756c185c5eb3abeb1c7b151fa467a33
Cédric Payet <cedricpayet.dev@gmail.com>
2024-11-12 20:53:36 +0100

    adding main.scss

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,71 +1,25 @@
-import React, { useState, useEffect } from 'react';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faFile, faEnvelope, faFileArrowDown, faMoon, faSun } from '@fortawesome/free-solid-svg-icons';
-import { faLinkedin } from '@fortawesome/free-brands-svg-icons';
-import classNames from 'classnames';
-import styles from './Header.module.scss'
+import { useState, useEffect } from "react";
+import classNames from "classnames";
+import HeaderControls from "./HeaderControls";
+import styles from "./Header.module.scss";
 
 function Header() {
-  const [scrolled, setScrolled] = useState(false);
-  const [isHovered, setIsHovered] = useState(false);
-  const [lightMode, setLightMode] = useState(true);
 
-  
-  const email = "cedricpayet.dev@gmail.com";
+  const [isScrolled, setIsScrolled] = useState(false);
 
-  const handleScroll = () => {
-    const isScrolled = window.scrollY > 0;
-    setScrolled(isScrolled);
-  };
-
-  const handleLightModeToggle = () => {
-    setLightMode(!lightMode);
-  }
-  
+  const handleScroll = () => setIsScrolled(window.scrollY > 0);
 
   useEffect(() => {
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
   }, []);
 
   return (
-    <header className={classNames(styles.header, { [styles.scrolled]: scrolled })}>
-        <h1 className={styles.header_title}>Portfolio</h1>
-          <ul className={styles.header_controls}>
-            <li>
-              <a href='https://www.linkedin.com/in/cédric-payet' target='_blank'>
-                <FontAwesomeIcon icon={faLinkedin} />
-              </a>
-            </li>
-            <li>
-              <a href='mailto:${email}'>
-                <FontAwesomeIcon icon={faEnvelope} />
-              </a>
-            </li>
-            <li
-              onMouseEnter={() => setIsHovered(true)}
-              onMouseLeave={() => setIsHovered(false)}
-            >
-              <a href="../../assets/react.svg" download="Cédric_Payet_CV.svg">
-                {isHovered ? (
-                  <FontAwesomeIcon icon={faFileArrowDown} />
-                ) : (
-                  <FontAwesomeIcon icon={faFile} />
-                )}
-              </a>
-            </li>
-            <li onClick={handleLightModeToggle}>
-              {lightMode ? (
-                <FontAwesomeIcon icon={faSun} />
-              ) : (
-                <FontAwesomeIcon icon={faMoon} />
-              )}
-          </li>
-          </ul>
+    <header className={classNames(styles.header, { [styles.scrolled]: isScrolled })}>
+      <h1 className={styles.header_title}>Portfolio</h1>
+      <HeaderControls/>
     </header>
-  )
+  );
 }
 
-export default Header
+export default Header;
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.scss'
 import App from './pages/Home/Home.tsx'
 import Header from './components/header/Header.tsx'
+import './styles/main.scss'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
