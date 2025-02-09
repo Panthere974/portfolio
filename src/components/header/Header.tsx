f2dd5358cd768a61423760a61cc9d5d2187164f6
Cédric Payet <cedricpayet.dev@gmail.com>
2025-02-10 00:02:23 +0100

    switch theme : done

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,8 +1,31 @@
+import { useEffect, useState } from "react";
 import { HashLink } from "react-router-hash-link";
-import {SunIcon} from "@radix-ui/react-icons"
+import {SunIcon, MoonIcon} from "@radix-ui/react-icons";
 import styles from "./Header.module.scss";
 
 function Header() {
+
+  const getInitialTheme = () => {
+    const localStorageTheme = localStorage.getItem("theme");
+    return localStorageTheme === "light";
+  };
+  
+  const [isLightTheme, setIsLightTheme] = useState(getInitialTheme);
+
+  useEffect(() => {
+    if (isLightTheme) {
+      document.documentElement.setAttribute("data-theme", "light");
+      localStorage.setItem("theme", "light");
+    } else {
+      document.documentElement.setAttribute("data-theme", "dark");
+      localStorage.setItem("theme", "dark");
+    }
+  }, [isLightTheme]);
+
+  function switchTheme() {
+    setIsLightTheme(!isLightTheme);
+  }
+
   return (
     <header className={styles.header}>
         <h1>Portfolio</h1>
@@ -10,7 +33,10 @@ function Header() {
             <HashLink className={styles.hashlink} smooth to="#about">À propos</HashLink>
             <HashLink className={styles.hashlink} smooth to="#skills">Compétences</HashLink>
             <HashLink className={styles.hashlink} smooth to="#projects">Projets</HashLink>
-            <SunIcon className={styles.header_nav_icon}/>
+            {isLightTheme ? 
+              <SunIcon className={styles.header_nav_icon} onClick={switchTheme}/> : 
+              <MoonIcon className={styles.header_nav_icon} onClick={switchTheme}/>
+            }
         </nav>
     </header>
   );
